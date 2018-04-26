2fbaf942ba437b48e8290acb9231dec73012d4e5
Keiko Yoshida <kitchen8nn@gmail.com>
2018-04-26 13:58:05 -0700

    implement documents#create function

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -5,11 +5,18 @@ class DocumentsController < ApplicationController
   end
 
   def create
+    @document = Document.new(document_params)
+    if @document.save
+      redirect_to @document
+    else
+      render 'new'
+    end
   end
 
   private
 
     def document_params
+      params.require(:document).permit(:title, :content)
     end
 
 end
