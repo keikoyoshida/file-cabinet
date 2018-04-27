523f79b8b54586650e45179c124ebd4fe83aa005
Keiko Yoshida <kitchen8nn@gmail.com>
2018-04-27 16:05:55 -0700

    set up for divise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root to: 'welcome#index'
 
   resources :documents, only: [:index, :new, :create, :show, :edit, :update]
   delete 'documents/:id', to: 'documents#destroy', as: :document_delete
