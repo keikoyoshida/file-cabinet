530caac5413b936b9d9370715bb24434542aad63
Keiko Yoshida <kitchen8nn@gmail.com>
2018-05-01 15:00:25 -0700

    change documents#index to show current user's documents

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -3,7 +3,7 @@ class DocumentsController < ApplicationController
   before_action :set_document, only: [:show, :edit, :update, :destroy]
 
   def index
-    @documents = Document.all.order('created_at DESC')
+    @documents = Document.where(user_id: current_user).order('created_at DESC')
   end
 
   def new
